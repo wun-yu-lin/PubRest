99269f1e3b6c4adace95d6f74ab2d9ae76b897e8
wun-yu-lin <t928394558@gmail.com>
2022-12-13 00:39:38 +0800

    add create file function

--- api_package/api_pubchem.py
+++ api_package/api_pubchem.py
@@ -1,8 +1,7 @@
-import json
 import logging
-import sys
-import time
 import urllib.request
+import os
+from pathlib import Path
 
 try:
     from urllib.error import HTTPError
@@ -20,8 +19,11 @@ __license__ = 'NCHU'
 API_BASE = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug'
 log = logging.getLogger('pubRest')
 log.addHandler(logging.NullHandler())
+
+    
 ##https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/2244/SDF
-def get_SDF_by_cid (domain="compound", namespace= "cid", cid_key = 1, ):
+
+def get_SDF_by_cid (domain="compound", namespace= "cid", cid_key = 1,path="./sdf" ):
     if isinstance(cid_key, int) == False:
         ValueError('please input CID (positive integer)')
         return
@@ -30,14 +32,47 @@ def get_SDF_by_cid (domain="compound", namespace= "cid", cid_key = 1, ):
     symbol = "/"
     
     apiurl = symbol.join((API_BASE,domain,namespace,cid_key_str,"SDF"))
-    print(apiurl)
+    
+    
+    ##if folder not exist, create folder
+    p = Path(path)
+    if(p.exists()==False):
+        os.mkdir(path)
+        
+    
+    
+    ##print(apiurl)
     try:
         log.debug('Request URL: %s', apiurl)
         with urllib.request.urlopen(apiurl) as response:
-            data = response.read().decode()
+            data_undecode = response.read()
+            data = data_undecode.decode()
+            #get compound name
+            index = 0
+            for x in data_undecode.splitlines():
+                index+=1
+
+                if (str(x)=="b'> <PUBCHEM_IUPAC_CAS_NAME>'"):
+                   compoundName = str(data_undecode.splitlines()[index]).split("'")[1]
+            
+        
+        
+        data_path = "{path}/{cid_key_str}_{compoundName}.sdf".format(path=str(path),cid_key_str = str(cid_key), compoundName =str(compoundName))
+        with open(data_path, 'w') as f:
+            f.write(str(data))
+        
+        
+        
+        
+        
         return data
     except HTTPError as e:
         ValueError("HTTPError")
     
     
+
+
+
+
+##test function
 get_SDF_by_cid(cid_key=2244)
--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 ##test
 from api_package import api_pubchem
 
-
-
-
 cid_key = 2244
+api_pubchem.get_SDF_by_cid(cid_key=2244)
 
-path = "{cid_key_str}.sdf".format(cid_key_str = str(cid_key))
-with open(path, 'w') as f:
-    f.write(str(api_pubchem.get_SDF_by_cid(cid_key=2244)))
+for x in range(1,1000):
+    api_pubchem.get_SDF_by_cid(cid_key=x)
